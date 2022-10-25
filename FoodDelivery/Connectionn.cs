db45f452c4c85d6933b92b69f354559857e53858
Leela05 <leelarenu05@gmail.com>
2022-10-25 16:21:03 +0530

    Add project files.

---
+++ FoodDelivery/Connectionn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FoodDelivery
+{
+    public class Connectionn
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+    }
+}
---
+++ FoodDelivery/User/Restaurant.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Text;
+
+namespace FoodDelivery.User
+{
+    public partial class Restaurant : System.Web.UI.Page
+    {
+
+
+        //SqlConnection con;
+        //SqlCommand cmd;
+        //SqlDataAdapter sda;
+        //DataTable dt;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+    }
+}
+       
